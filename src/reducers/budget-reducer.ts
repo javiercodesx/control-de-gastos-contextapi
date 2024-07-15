091d02373fb535a776be8a877eeb13c65d877b5d
javierspr <learn.javier@gmail.com>
2024-07-15 20:45:38 -0300

    add show modal

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,17 +1,20 @@
 export type BudgetActions = 
-    {type: "add-budget", payload: {budget: number}}
+    {type: "add-budget", payload: {budget: number}} |
+    {type: "show-modal"} 
 
 export type BudgetState = {
     budget: number
+    modal: boolean
 }
 
 export const initialState : BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false
 }
 
 export const budgetReducer = (
     state : BudgetState = initialState,
-    action : BudgetActions    
+    action : BudgetActions      
     ) => {
 
     if(action.type === "add-budget") {
@@ -20,6 +23,12 @@ export const budgetReducer = (
             budget: action.payload.budget
         }
     }
-    
+
+    if(action.type === "show-modal"){
+        return{
+            ...state,
+            modal: true
+        }
+    }
     return state
 }
